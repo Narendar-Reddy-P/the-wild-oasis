b16ccaafe92bf8b3ed19c71ee5dd3eb722550001
narendar-reddy-p <narendar.reddy.pucchakayala@gmail.com>
2026-05-06 21:06:04 +0530

    Built Sidebar and Main Nav

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,12 +1,22 @@
 import styled from "styled-components";
+import Logo from "./Logo";
+import MainNav from "./MainNav";
 
 const StyledSidebar = styled.aside`
   background-color: aliceblue;
   padding: 3.2rem 3.4rem;
   border-right: 1px solid var(--color-grey-0);
   grid-row: 1/-1;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 
 export default function Sidebar() {
-  return <StyledSidebar>SIDEBAR</StyledSidebar>;
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
 }
