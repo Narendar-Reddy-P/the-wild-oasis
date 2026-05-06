6244f5b930cbee0ca041cbb9a247913daca06724
narendar-reddy-p <narendar.reddy.pucchakayala@gmail.com>
2026-05-06 06:18:49 +0530

    included styled components

---
+++ src/ui/Heading.jsx
@@ -0,0 +1,35 @@
+import styled, { css } from "styled-components";
+
+// let a = 10;
+// let b = 5;
+// const test = css`
+//   text-align: center;
+//   ${a > b && "background-color: yellow"}
+// `;
+
+const Heading = styled.h1`
+  ${(props) =>
+    props.as === "h1" &&
+    css`
+      font-size: 3rem;
+      font-weight: 600;
+    `}
+
+  ${(props) =>
+    props.as === "h2" &&
+    css`
+      font-size: 2rem;
+      font-weight: 600;
+    `}
+
+  ${(props) =>
+    props.as === "h3" &&
+    css`
+      font-size: 1rem;
+      font-weight: 600;
+    `}
+
+  font-weight: 600;
+`;
+
+export default Heading;
